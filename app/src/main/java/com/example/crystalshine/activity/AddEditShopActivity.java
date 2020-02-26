0c81b806a5732c11e66dbde52a6a2e0d0de45579
ThiriWai26 <chothiri27@gmail.com>
2020-02-26 15:54:28 +0630

    UIall

---
+++ app/src/main/java/com/example/crystalshine/activity/AddEditShopActivity.java
@@ -0,0 +1,16 @@
+package com.example.crystalshine.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.crystalshine.R;
+
+public class AddEditShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_shop);
+    }
+}
